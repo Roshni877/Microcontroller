8546c9725d5e091cd023883d783a364733ad4db6
Roshni <roshni.23cs122@sode-edu.in>
2025-05-19 07:09:46 +0530

    Add files via upload

---
+++ lab10.asm
@@ -0,0 +1,13 @@
+	area lab10,code,readonly
+start
+;disabling
+	mrs r0, cpsr ;11010011=D3
+	BIC r0, r0, #0x80 ;01010011=53
+	msr cpsr_c, r0;
+;enabling
+	mrs r0, cpsr ;
+	ORR r0, r0, #0x80
+	msr cpsr_c, r0;
+;stopping
+	bx lr
+End
